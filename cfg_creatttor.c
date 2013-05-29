fa79b2d42a6413dcf79430d707413fc23d0c46b8
michaelfaisst <michael.faisst@students.fhv.at>
2013-05-29 16:30:10 +0800

    asdf

---
+++ cfg_creatttor.c
@@ -0,0 +1,251 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+#include "graph/block.h"
+#include "graph/graph.h"
+
+#include "cfg_creatttor.h"
+
+extern struct PROGRAM *root;
+
+typedef struct ListNode {
+	struct ListNode* next;
+	struct Block* block;
+} Listnode;
+
+
+FILE* cfgStream;
+struct CFG *cfg;
+struct Listnode *currNode;
+
+
+/*
+	Generates the AST text representation and the symbol table
+	For both files seperate streams are needed - their opening/closing should be handled from the outside
+*/
+void generateCFG(FILE *cfgStreamPar)
+{
+	cfgStream = cfgStreamPar;
+	initCFG(cfg);
+	
+	struct FUNCTION *currentFunc = root->FuncList;
+
+	while (currentFunc != NULL)
+	{
+		fprintf(cfgStream, "%s\n", currentFunc->ID);
+		gotoFunction(currentFunc);
+		currentFunc = currentFunc->prev;
+	}
+}
+
+void gotoDeclaration(struct DECLARATION* decl)
+{
+	struct IDENTIFIER *currIdent = decl->ilist;
+
+	while (currIdent != NULL)
+	{
+		gotoIdentifier(currIdent);
+		currIdent = currIdent->prev;
+	}
+}
+
+void gotoFunction(struct FUNCTION *func)
+{
+	struct PARAMETER *currPar = func->ParamList;
+
+	while (currPar != NULL)
+	{
+		gotoParameter(currPar);
+		currPar = currPar->prev;
+	}
+
+	gotoCompound(func->CStmt);
+
+}
+
+void gotoParameter(struct PARAMETER *par)
+{
+	gotoIdentifier(par->id);	
+}
+
+void gotoCompound(struct COMPOUNDSTMT *comp)
+{	
+	struct DECLARATION *currDecl = comp->DeclList;
+
+	/*while (currDecl != NULL)
+	{
+		gotoDeclaration(currDecl);
+		currDecl = currDecl->prev;
+	}*/
+
+	struct STMT *currStmt = comp->StmtList;	
+
+	struct Block* currBlock = createBlock();
+
+	while (currStmt != NULL)
+	{
+		currBlock = gotoStatement(currStmt, 0, currBlock);
+		currStmt = currStmt->prev;
+	}
+}
+
+Block* gotoStatement(struct STMT *stmt, int isAlreadyDeeper, currBlock)
+{
+
+	Block* returnBlock = currBlock;	
+
+	/* statement needs to be switched, because multiple possibilities are available */
+	switch (stmt->e_stmt)
+	{
+		case eAssign:
+			addStatementToBlock(currBlock, stmt);
+			gotoAssign(stmt->stmt.assign_s);
+			break;
+		case eCall:
+			addStatementToBlock(currBlock, stmt);
+			gotoCall(stmt->stmt.call_s);
+			break;
+		case eRet:
+			addStatementToBlock(currBlock, stmt);
+			gotoExpr(stmt->stmt.return_s);
+			break;
+		case eWhile:
+			gotoWhile(stmt->stmt.while_s);
+			break;
+		case eDoWhile:
+			gotoDoWhile(stmt->stmt.dowhile_s);
+			break;
+		case eFor:
+			gotoFor(stmt->stmt.for_s);
+			break;
+		case eIf:
+			// add Expr of the if to the current block....no idea how to do
+			gotoIf(stmt->stmt.if_s, currBlock);
+			break;
+		case eCompound:
+			gotoCompound(stmt->stmt.compound_s);
+			break;
+		case eSemi:
+			break;
+	}
+
+	return returnBlock;
+}
+
+void gotoExpr(struct EXPR *expr)
+{
+	/* expressions need to be switched, because multiple possibilities are available */
+	switch(expr->e_expr)
+	{
+		case eUnop:
+			gotoUnop(expr->expression.unop_expr);
+			break;
+		case eBinop:
+			gotoBinop(expr->expression.binop_expr);
+			break;
+		case eCallExpr:
+			gotoCall(expr->expression.call_expr);
+			break;
+		case eIntnum:
+			//gotoInt(expr->expression.intnum);
+			break;
+		case eFloatnum:
+			//gotoFloat(expr->expression.floatnum);
+			break;
+		case eId:
+			gotoIDs(expr->expression.ID_expr);
+			break;
+		case eExpr:
+			gotoExpr(expr->expression.bracket);
+			break;
+	}
+}
+
+void gotoUnop(struct UNOP *un)
+{
+	gotoExpr(un->expr);
+}
+
+void gotoBinop(struct BINOP *bin)
+{
+	gotoExpr(bin->expr1);
+	gotoExpr(bin->expr2);
+}
+
+void gotoIDs(struct IDs *ids)
+{
+	if (ids->expr != NULL)
+	{
+		gotoExpr(ids->expr);
+	}
+}
+
+void gotoArgument(struct ARGLIST *arg)
+{
+	gotoExpr(arg->expr);
+}
+
+void gotoAssign(struct ASSIGN *assign)
+{
+	if (assign->index != NULL)
+	{
+		gotoExpr(assign->index);
+	}
+
+	gotoExpr(assign->expr);
+}
+
+void gotoCall(struct CALL *call)
+{
+	struct ARGLIST *currArg = call->arg;
+
+	while (currArg != NULL)
+	{
+		gotoArgument(currArg);
+		currArg = currArg->prev;
+	}
+}
+
+void gotoWhile(struct WHILEs *whil)
+{
+	gotoExpr(whil->condition);
+	gotoStatement(whil->stmt, 1);
+}
+
+void gotoDoWhile(struct DOWHILEs *dowhile)
+{
+	gotoStatement(dowhile->stmt, 1);
+	gotoExpr(dowhile->condition);
+}
+
+void gotoFor(struct FORs *fr)
+{
+	gotoAssign(fr->init);
+	gotoExpr(fr->condition);
+	gotoAssign(fr->next);
+	gotoStatement(fr->stmt, 1);
+}
+
+void gotoIf(struct IFs *iff, Block* currentBlock)
+{
+	gotoExpr(iff->condition);
+
+	Block* ifBlock = createBlock();
+	addConnection(cfg, currentBlock->nr, ifBlock->nr);
+
+	gotoStatement(iff->if_s, 1, ifBlock);
+
+	if (iff->else_s != NULL)
+	{
+		Block* elseBlock = createBlock();
+		addConnection(cfg, currentBlock->nr, elseBlock->nr);
+
+		gotoStatement(iff->else_s, 1 elseBlock);
+	}
+}
+
+void gotoIdentifier(struct IDENTIFIER* identifier)
+{	
+}
+
+
